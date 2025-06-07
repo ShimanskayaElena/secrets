5ce54052bcf3d2991ddccd422096e97ed0b5b091
aa.shymanskaya <aa.shymanskaya@lab.vebtech.by>
2025-06-07 12:50:02 +0300

    Auto-generated commit

--- chunk-I7XIASZ4.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-JXDN2EZQ.js";import"./chunk-OGXTR33P.js";export{a as CounterComponent};
---
+++ chunk-R3XF56DR.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-MSLQ4FJL.js";import"./chunk-FVNDCGGX.js";import"./chunk-OGXTR33P.js";export{a as CounterComponent};
